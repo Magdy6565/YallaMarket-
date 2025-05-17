a547298fc3634f5e147446b8b520794d93b605df
Magdy6565 <youssefmigzo555@gmail.com>
2025-05-17 20:19:28 +0300

    added login and signup func with JWT but supabase not connected successfully

---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.repository;
+
+
+import com.example.demo.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    // Find user by email
+    Optional<User> findByEmail(String email);
+    // Find user by the verification code they would eneter
+    Optional<User> findByVerificationCode(String verificationCode);
+}
